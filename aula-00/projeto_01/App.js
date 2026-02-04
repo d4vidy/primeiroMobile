6688ab24d746aeb9e84cdbf2210ce2e1fff89344
David Yehud de Jesus Chaves <130412059+d4vidy@users.noreply.github.com>
2026-02-04 23:25:55 +0000

    primeira impressão do react native

---
+++ aula-00/projeto_01/App.js
@@ -0,0 +1,53 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Tentativa de alteração</Text>
+      <View style={{backgroundColor:'red'}}><PizzaTranslator/></View>
+      
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20
+  },
+  view: {
+    width: 20,
+    height: 10
+  }
+});
+
+const PizzaTranslator = () => {
+  const [text, setText] = useState('');
+  return (
+    <View style={{flex: 1, justifyContent: 'center'}}>
+      <TextInput
+        placeholder="Type here to translate!"
+        onChangeText={newText => setText(newText)}
+        defaultValue={text}
+        style={{
+          height: 100,
+          padding: 5,
+          marginHorizontal: 8,
+          borderWidth: 1,
+        }}
+      />
+      <Text style={{padding: 10, fontSize: 42}}>
+        {text
+          .split(' ')
+          .map(word => word && '🍕')
+          .join(' ')}
+      </Text>
+    </View>
+  );
+};
