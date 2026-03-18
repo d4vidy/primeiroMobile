d86bb420f5e95429896f87c871f690e8da7d4cda
David Yehud de Jesus Chaves <130412059+d4vidy@users.noreply.github.com>
2026-03-18 00:38:00 +0000

    alterações na atividade pratica, inserção da navegação por abas

--- aulaPratica_Modal/PraticaModal/App.js
+++ aulaPratica_Modal/PraticaModal/App.js
@@ -1,10 +1,32 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import CustomModalScreen from './components/CustomModal';
+import { NavigationContainer, TabActions } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <CustomModalScreen animation="slide" themeColor="#2196f3" />
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={{
+            headerShown: false,
+            tabBarActiveTintColor: '#000',
+            tabBarLabelStyle: {fontSize: 13, fontWeight: 'bold'}
+          }}
+        >
+          <Tab.Screen name="SLIDE">
+            {() => <CustomModalScreen animation="slide" themeColor="#2196f3" />}
+          </Tab.Screen>
+          <Tab.Screen name="FADE">
+            {() => <CustomModalScreen animation="fade" themeColor="#4Caf50" />}
+          </Tab.Screen>
+          <Tab.Screen name="NONE">
+            {() => <CustomModalScreen animation="none" themeColor="#ff9800" />}
+          </Tab.Screen>
+        </Tab.Navigator>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
